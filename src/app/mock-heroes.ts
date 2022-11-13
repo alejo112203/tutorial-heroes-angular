22d1118dc9cc890159ee734a01de2923b68428ff
Alejandro Guerrero <alejo112203@gmail.com>
2022-11-13 12:06:46 -0500

    listado de heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 12, name: "Spiderman"},
+    {id: 13, name: "Ironman"},
+    {id: 14, name: "Thor"},
+    {id: 15, name: "Wolverine"},
+    {id: 16, name: "Deadpool"},
+    {id: 17, name: "Capitan America"},
+    {id: 18, name: "Superman"},
+]
